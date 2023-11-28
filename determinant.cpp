cc864f162f26837941d92161ccb37b5ba5cc2c94
Davit-M <87268065+DaviDM2005@users.noreply.github.com>
2023-11-29 02:38:14 +0400

    Determinant-cpp

---
+++ determinant.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int matrix[3][3]; 
+    int count = 1; 
+
+    // Input - all elements
+    cout << "Enter elements of 3x3 matrix:" << endl;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            cout << "Enter element " << count << ": ";
+            cin >> matrix[i][j];
+            count++;
+        }
+    }
+
+
+    cout << endl;
+
+    // Output - Your 3x3 array
+    cout << "The 3x3 matrix entered is:" << endl;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            cout << matrix[i][j] << " ";
+        }
+        cout <<  endl;
+    }
+
+    cout << endl;
+
+
+    // Define the determinant
+
+    int detValue = 0;
+
+    for (int i = 0; i < 3; ++i) {
+        int LeftToRight_value = 1;
+        int RightToLeft_value = 1;
+
+        for (int j = 0; j < 3; ++j) {
+            LeftToRight_value *= matrix[j][(i + j) % 3];
+            RightToLeft_value *= matrix[2 - j][(i + j) % 3];
+        }
+
+        detValue += LeftToRight_value - RightToLeft_value;
+
+    }
+
+    // Output - Determinant
+
+    cout << "Determinant: " << detValue << endl;
+
+    return 0;
+}
