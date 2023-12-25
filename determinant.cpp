ebabd44c8e81b0c65871f655f801a82691ea77e2
Davit-M <87268065+DaviDM2005@users.noreply.github.com>
2023-12-26 01:53:04 +0400

    Update determinant.cpp

--- determinant.cpp
+++ determinant.cpp
@@ -33,19 +33,25 @@ int main() {
 
     // Define the determinant
 
+    // int detValue = 0;
+
+    // for (int i = 0; i < 3; ++i) {
+    //     int LeftToRight_value = 1;
+    //     int RightToLeft_value = 1;
+
+    //     for (int j = 0; j < 3; ++j) {
+    //         LeftToRight_value *= matrix[j][(i + j) % 3];
+    //         RightToLeft_value *= matrix[2 - j][(i + j) % 3];
+    //     }
+
+    //     detValue += LeftToRight_value - RightToLeft_value;
+
+    // }
+
     int detValue = 0;
 
-    for (int i = 0; i < 3; ++i) {
-        int LeftToRight_value = 1;
-        int RightToLeft_value = 1;
-
-        for (int j = 0; j < 3; ++j) {
-            LeftToRight_value *= matrix[j][(i + j) % 3];
-            RightToLeft_value *= matrix[2 - j][(i + j) % 3];
-        }
-
-        detValue += LeftToRight_value - RightToLeft_value;
-
+    for (int i = 0; i < 3; i++) {
+        detValue += matrix[0][i] * (matrix[1][(i + 1) % 3] * matrix[2][(i + 2) % 3] - matrix[1][(i + 2) % 3] * matrix[2][(i + 1) % 3]);
     }
 
     // Output - Determinant
